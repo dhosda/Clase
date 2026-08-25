12cd0dfcbebde54e1f518fad947af253c5762e3c
dhosda <dhostyndavalos09@gmail.com>
2026-08-25 07:24:29 -0600

    Nuevo

---
+++ Circulo.java
@@ -0,0 +1,13 @@
+class Circulo extends Figura {
+     private double radio;
+
+     public Circulo(double radio) {
+         this.radio = radio;
+     }
+
+     @Override
+     double calcularArea() {
+         return Math.PI * radio * radio;
+     }
+ }
+ 
